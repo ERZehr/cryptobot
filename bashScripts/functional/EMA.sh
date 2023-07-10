560417df0638c93d7dfdedfa03c67ef8a4cf73f0
Evan Zehr <erzehr11@gmail.com>
2023-07-09 21:05:17 -0400

    Fixed EMA if works now

--- bashScripts/functional/EMA.sh
+++ bashScripts/functional/EMA.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+PERIOD=$3
+LIMIT=$4
+while [ "$PERIOD" -gt "$LIMIT" ]; do
+    echo "The data window must be longer than the period."
+    read -p "Enter the period: " PERIOD
+    read -p "Enter the limit: " LIMIT
+done
+
 cd ../../pythonScripts/functional
-filename=$(./EMA.py "$1" "$2" "$3" "$4" "$5")
+filename=$(./EMA.py "$1" "$2" "$PERIOD" "$LIMIT" "$5")
 mv "$filename" ../../data_files
--- pythonScripts/functional/EMA.py
+++ pythonScripts/functional/EMA.py
@@ -8,45 +8,44 @@ ASSET = sys.argv[1]  # Cryptocurrency
 CURRENCY = sys.argv[2]  # Reference currency (Typically USD)
 PERIOD = int(sys.argv[3])  # Moving average period
 LIMIT = int(sys.argv[4])  # Desired number of data points
-
-while PERIOD >= LIMIT:
-    print("The data window must be longer than the period.")
-    PERIOD = int(input("Enter the period: "))
-    LIMIT = int(input("Enter the number of desired data points: "))
-
 RANGE = sys.argv[5]  # Day, hour, minute
 
-DATAQUERY = str(PERIOD + LIMIT)  # Number of data points to query
+subprocess.run(["bash", "../../bashScripts/functional/getData.sh", ASSET, CURRENCY, str(LIMIT), RANGE])  # Run getData.sh to create a data file
 
-subprocess.run(["bash", "../../bashScripts/functional/getData.sh", ASSET, CURRENCY, DATAQUERY, RANGE])  # Run getData.sh to create a data file
-
-FILENAME = ASSET + "_last_" + DATAQUERY + "_" + RANGE + "s"  # Create a filename
+FILENAME = ASSET + "_last_" + str(LIMIT) + "_" + RANGE + "s"  # Create a filename
 data_file_path = os.path.join("..", "..", "data_files", FILENAME)  # Create the file path
 
 data = []  # List to store the price data
 moving_averages = []  # List to store the moving averages
+dates = []  # List to store the dates
+times = []  # List to store the times
 
-# Read the data from the file and extract the prices
+# Read the data from the file and extract the prices, dates, and times
 with open(data_file_path, 'r') as file:
     for line in file:
         parts = line.strip().split(', ')
+        datetime = parts[0].split(' ')  # Splitting the date and time
+        date = datetime[0]
+        time = datetime[1]
         price = float(parts[1])  # Extract the price from the line
         data.append(price)  # Add the price to the data list
+        dates.append(date)  # Add the date to the dates list
+        times.append(time)  # Add the time to the times list
 
 # Calculate the exponential moving averages
 alpha = 2 / (PERIOD + 1)  # Smoothing factor
 ema = data[0]  # Initial EMA value
+moving_averages.append(ema)  # Set the first value into the list
 
 for i in range(1, len(data)):
     ema = alpha * data[i] + (1 - alpha) * ema  # Calculate the EMA
     moving_averages.append(round(ema, 2))  # Add the EMA to the moving_averages list
 
-FILENAME2 = ASSET + "_last_" + DATAQUERY + "_" + RANGE + "s_EMA"  # Create a filename
+FILENAME2 = ASSET + "_" + str(PERIOD) + "_" + RANGE + "_period_EMA_against_last_" + str(LIMIT) + "_" + RANGE + "s" # create filename
 
-with open(FILENAME2, 'w') as file:  # Write the exponential moving averages to a file
-    file.write("Exponential Moving Averages:\n")
+with open(FILENAME2, 'w') as file:
     for i, average in enumerate(moving_averages, start=0):
-        file.write(f"Period {i}: {average}\n")
+        file.write(f"{dates[i]}, {times[i]}, {average}\n")
 
 os.environ["FILENAME"] = FILENAME  # Set the value of FILENAME2 as an environment variable
 subprocess.run(["bash", "-c", "rm -f ../../data_files/$FILENAME"])  # Run the subprocess to remove the file
